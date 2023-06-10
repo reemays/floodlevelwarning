302a58468f04b0556b24aebc0cb753281035dada
Syameer <avorerstina@gmail.com>
2023-06-10 22:00:17 +0800

    index update

--- index.py
+++ index.py
@@ -1,6 +1,7 @@
 from tensorflow.keras.models import load_model
 import pandas as pd
 import streamlit as st
+import requests
 
 st.title("Flood Level Prediction")
 
@@ -11,7 +12,18 @@ user_input = pd.DataFrame([[intensity]], columns=["intensity"])
 model = load_model('model.h5')
 model.built = True
 
-if st.button("Predict"):
+if st.button("Predict"): 
+    url = ('https://api.thingspeak.com/channels/2135773/feeds.json?api_key=5ISF03WS3CO2HYSD&results=2')
+    print(url)
+    response = requests.get(url)
+    print(f"Request returned {response.status_code} : '{response.reason}'")
+    msg=requests.get(url)
+    msg=msg.json()['feeds'][-1]['field2']
+    print("Last updated water level: \n\n"+str(msg))
+    level = float(msg)
+    prediction =  [level, 3.0]
+    pred = model.predict([prediction])
+    pred_int = int(pred*100)
 	st.write("Flood Probability: ", pred_int,'%')
 
 pd.show_versions()
