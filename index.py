481af836bf713bb19fb20fe6633933cec090e3f0
Syameer <avorerstina@gmail.com>
2023-06-10 21:54:58 +0800

    Big things

--- index.py
+++ index.py
@@ -1,14 +1,14 @@
-import joblib
+from tensorflow.keras.models import load_model
 import pandas as pd
 import streamlit as st
 
 st.title("Flood Level Prediction")
 
-intensity = st.selectbox( 'What is the intensity value?', ('0', '1', '2','3'))
+intensity = st.selectbox( 'RAIN INTENSITY (0 for None, 1 for Light, 2 for Moderate, 3 for Heavy)', ('0', '1', '2','3'))
 
 user_input = pd.DataFrame([[intensity]], columns=["intensity"])
 
-model = joblib.load('model.pkl')
+model = load_model('model.h5')
 model.built = True
 
 if st.button("Predict"):
