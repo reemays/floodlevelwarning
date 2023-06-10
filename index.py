309379c6d5b39f5e9d36ca190c70140b2b99a5c0
Syameer <avorerstina@gmail.com>
2023-06-10 21:49:07 +0800

    Update index.py

--- index.py
+++ index.py
@@ -9,6 +9,7 @@ intensity = st.selectbox( 'What is the intensity value?', ('0', '1', '2','3'))
 user_input = pd.DataFrame([[intensity]], columns=["intensity"])
 
 model = joblib.load('model.pkl')
+model.built = True
 
 if st.button("Predict"):
 	st.write("Flood Probability: ", pred_int,'%')
