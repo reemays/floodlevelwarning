0f031a6ee19a92ebcb290d9ef8441361d1dbfef6
Syameer <avorerstina@gmail.com>
2023-06-10 21:39:31 +0800

    First Upload
    Trying its best

---
+++ index.py
@@ -0,0 +1,16 @@
+import joblib
+import pandas as pd
+import streamlit as st
+
+st.title("Flood Level Prediction")
+
+intensity = st.selectbox( 'What is the intensity value?', ('0', '1', '2','3'))
+
+user_input = pd.DataFrame([[intensity]], columns=["intensity"])
+
+model = joblib.load('model.pkl')
+
+if st.button("Predict"):
+	st.write("Flood Probability: ", pred_int,'%')
+
+pd.show_versions()
