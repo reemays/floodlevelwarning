97248779d1e0c2ca3c6e80965079f1744370e26c
Syameer <avorerstina@gmail.com>
2023-06-10 22:12:28 +0800

    Update index.py

--- index.py
+++ index.py
@@ -13,7 +13,7 @@ user_input = float(intensity)
 model = load_model('model.h5')
 model.built = True
 
-def func_predict():
+def func_level():
     url = ('https://api.thingspeak.com/channels/2135773/feeds.json?api_key=5ISF03WS3CO2HYSD&results=2')
     print(url)
     response = requests.get(url)
@@ -22,12 +22,17 @@ def func_predict():
     msg=msg.json()['feeds'][-1]['field2']
     print("Last updated water level: \n\n"+str(msg))
     level = float(msg)
-    prediction =  [level, user_input]
+    return level
+
+def func_predict():
+    func_level()
+    prediction =  [func_level(), user_input]
     pred = model.predict([prediction])
     pred_int = int(pred*100)
     return pred_int
 
 if st.button("Predict"): 
+    st.write("Current water level: ", func_level())  
 	st.write("Flood Probability: ", func_predict(),'%')
     
 pd.show_versions()
